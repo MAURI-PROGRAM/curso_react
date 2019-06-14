52319f50bcfbd958c3d4d04a6129d4af41980499
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-06-14 17:46:03 -0500

    primer componente

---
+++ basicoreact/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import PrimerComponente from "./componente/PrimerComponente";
+
+function App() {
+  const empleado = {
+    nombre: "Michael Merchan",
+    trabajo: "programador"
+  };
+  return <Fragment />;
+}
+
+export default App;
---
+++ basicoreact/src/componente/PrimerComponente.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const PrimerComponent = () => <h1>Hola mundo</h1>;
+
+export default PrimerComponent;
