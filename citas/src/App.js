0cf68d5eeaae953a6761dd26ab31e8441e2f3ebc
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-06-24 12:47:17 -0500

    inicio de proyecto citas

---
+++ citas/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [citas, guardarcitas] = useState([]);
+
+  return (
+    <div>
+      <h1>hola</h1>
+    </div>
+  );
+};
+
+export default App;
