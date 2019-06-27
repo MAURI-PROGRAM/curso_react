8b755033d43ae6a4f27a8766c02bc6ec7cd10708
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-06-27 18:08:14 -0500

    enviando informacion mediante la api por el metodo post

--- routercrudreact/src/components/AgregarProducto.js
+++ routercrudreact/src/components/AgregarProducto.js
@@ -1,17 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import Error from "./Error";
+import axios from "axios";
+
 const AgregarProducto = () => {
+  const [nombrePlatillo, setnombrePlatillo] = useState("");
+  const [precioPlatillo, setprecioPlatillo] = useState("");
+  const [categoria, setcategoria] = useState("");
+  const [error, seterror] = useState(false);
+  const leerValorCategoria = e => {
+    setcategoria(e.target.value);
+  };
+  const agregarProducto = async e => {
+    e.preventDefault();
+    if (nombrePlatillo === "" || precioPlatillo === "" || categoria === "") {
+      seterror(true);
+      return;
+    } else {
+      seterror(false);
+      try {
+        const resultado = await axios.post("http://localhost:4000/restaurant", {
+          nombrePlatillo,
+          precioPlatillo,
+          categoria
+        });
+        console.log(resultado);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
   return (
     <div className="col-md-8 mx-auto ">
       <h1 className="text-center">Agregar Nuevo Producto</h1>
-
-      <form className="mt-5">
+      {error ? <Error mensaje="todos los campos son obligatorios" /> : null}
+      <form className="mt-5" onSubmit={agregarProducto}>
         <div className="form-group">
           <label>Nombre Platillo</label>
+
           <input
             type="text"
             className="form-control"
             name="nombre"
             placeholder="Nombre Platillo"
+            onChange={e => setnombrePlatillo(e.target.value)}
           />
         </div>
 
@@ -22,6 +53,7 @@ const AgregarProducto = () => {
             className="form-control"
             name="precio"
             placeholder="Precio Platillo"
+            onChange={e => setprecioPlatillo(e.target.value)}
           />
         </div>
 
@@ -33,6 +65,7 @@ const AgregarProducto = () => {
               type="radio"
               name="categoria"
               value="postre"
+              onChange={leerValorCategoria}
             />
             <label className="form-check-label">Postre</label>
           </div>
@@ -42,6 +75,7 @@ const AgregarProducto = () => {
               type="radio"
               name="categoria"
               value="bebida"
+              onChange={leerValorCategoria}
             />
             <label className="form-check-label">Bebida</label>
           </div>
@@ -52,6 +86,7 @@ const AgregarProducto = () => {
               type="radio"
               name="categoria"
               value="cortes"
+              onChange={leerValorCategoria}
             />
             <label className="form-check-label">Cortes</label>
           </div>
@@ -62,6 +97,7 @@ const AgregarProducto = () => {
               type="radio"
               name="categoria"
               value="ensalada"
+              onChange={leerValorCategoria}
             />
             <label className="form-check-label">Ensalada</label>
           </div>
