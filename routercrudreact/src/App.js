31f469b4084f7bb09f26abfa2e3360075dfdad7d
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-06-27 15:06:57 -0500

    uso de link y navlink para que se resalte lo seleccionado

--- routercrudreact/src/App.js
+++ routercrudreact/src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Productos from "./components/Productos";
 import EditarProducto from "./components/EditarProducto";
 import AgregarProducto from "./components/AgregarProducto";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/nuevo-producto" component={AgregarProducto} />
-        <Route exact path="/productos" component={Productos} />
-        <Route exact path="/productos/editar/:id" component={EditarProducto} />
-      </Switch>
+      <Header />
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/nuevo-producto" component={AgregarProducto} />
+          <Route exact path="/productos" component={Productos} />
+          <Route
+            exact
+            path="/productos/editar/:id"
+            component={EditarProducto}
+          />
+        </Switch>
+      </main>
     </Router>
   );
 };
