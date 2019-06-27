6358fb48fced38189753b2b8dc24ba6a3d004610
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-06-27 13:32:25 -0500

    generando las rutas

--- routercrudreact/src/App.js
+++ routercrudreact/src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Rou } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Productos from "./components/Productos";
+import EditarProducto from "./components/EditarProducto";
+import AgregarProducto from "./components/AgregarProducto";
+
 const App = () => {
-  return <div className="App">s</div>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/nuevo-producto" component={AgregarProducto} />
+        <Route exact path="/productos" component={Productos} />
+        <Route exact path="/productos/editar/:id" component={EditarProducto} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
