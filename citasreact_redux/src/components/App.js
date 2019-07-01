bc6c02cccead6dc06a66769d6cc84c32c4b2a6f1
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-07-01 16:32:14 -0500

    definiendo los reducer y los action

--- citasreact_redux/src/components/App.js
+++ citasreact_redux/src/components/App.js
@@ -1,78 +1,72 @@
-import React, { Component } from 'react';
-import Header from './Header';
-import AgregarCita from './AgregarCita';
-import ListaCitas from './ListaCitas';
+import React, { Component } from "react";
+import Header from "./Header";
+import AgregarCita from "./AgregarCita";
+import ListaCitas from "./ListaCitas";
+import { Provider } from "react-redux";
+import store from "../store";
 
 class App extends Component {
-
   state = {
     citas: []
-  }
-  
+  };
+
   componentDidUpdate() {
-      localStorage.setItem( 
-          'citas', 
-          JSON.stringify( this.state.citas ) 
-      );
+    localStorage.setItem("citas", JSON.stringify(this.state.citas));
   }
 
   componentDidMount() {
-      const citasLS = localStorage.getItem( 'citas' );
-      if(citasLS) {
-          this.setState({
-              citas: JSON.parse(citasLS)
-          });
-      }   
-  }
-
-  crearCita = (infoCita) => {
-  
-      // tomar una copia del state actual 
-      const citas = {...this.state.citas};
-
-      // agregar el gasto a la variable
-      citas[`citas${Date.now()}`] = infoCita;
-
-      // agregar el gasto a etate
+    const citasLS = localStorage.getItem("citas");
+    if (citasLS) {
       this.setState({
-          citas
+        citas: JSON.parse(citasLS)
       });
+    }
   }
 
-  borrarCita = (id) => {
-        // leer state
-        const citas = {...this.state.citas}
-        // borrarlo del objeto
-        delete citas[id];
+  crearCita = infoCita => {
+    // tomar una copia del state actual
+    const citas = { ...this.state.citas };
 
-        console.log(citas);
-        // actualizar state
-        this.setState({
-             citas
-        })
+    // agregar el gasto a la variable
+    citas[`citas${Date.now()}`] = infoCita;
 
-  }
+    // agregar el gasto a etate
+    this.setState({
+      citas
+    });
+  };
+
+  borrarCita = id => {
+    // leer state
+    const citas = { ...this.state.citas };
+    // borrarlo del objeto
+    delete citas[id];
+
+    console.log(citas);
+    // actualizar state
+    this.setState({
+      citas
+    });
+  };
 
   render() {
     return (
-      <div className="container">
-        <Header 
-          titulo={'Administrador de Pacientes Veterinaria'}
-        />
-        <div className="row">
-          <div className="col-md-6">
-              <AgregarCita
-                  crearCita={this.crearCita}
-              />
-          </div>
-          <div className="col-md-6">
+      <Provider store={store}>
+        <div className="container">
+          <Header titulo={"Administrador de Pacientes Veterinaria"} />
+          <div className="row">
+            <div className="col-md-6">
+              <AgregarCita crearCita={this.crearCita} />
+            </div>
+            <div className="col-md-6">
               <ListaCitas
-                citas = {this.state.citas}
-                borrarCita = {this.borrarCita }
+                citas={this.state.citas}
+                borrarCita={this.borrarCita}
               />
+            </div>
           </div>
         </div>
-      </div>
+      </Provider>
     );
   }
 }
---
+++ citasreact_redux/src/reducers/citasReducer.js
@@ -0,0 +1,17 @@
+import { MOSTRAR_CITAS, AGREGAR_CITA, BORAR_CITA } from "../actions/types";
+
+const initialState = {
+  citas: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case MOSTRAR_CITAS:
+      return {
+        ...state
+      };
+      break;
+    default:
+      break;
+  }
+}
