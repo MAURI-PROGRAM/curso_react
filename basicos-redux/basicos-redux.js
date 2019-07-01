a12463c2f7ec7eb74faaa269c01d633d2b66558e
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2019-07-01 12:51:10 -0500

    utilizadon dispatch en el redux

--- basicos-redux/basicos-redux.js
+++ basicos-redux/basicos-redux.js
@@ -9,11 +9,28 @@ const stateInicial = {
 //reducer
 //state y accion
 const reducerPrincipal = (state = stateInicial, action) => {
+  if (action.type === "AGREGAR_USUARIO") {
+    return {
+      ...state,
+      usuarios: action.nombre
+    };
+  }
+  if (action.type === "MOSTRAR_USUARIOS") {
+    return {
+      ...state
+    };
+  }
   return state;
 };
 
 //toma 3 paremetros: reducer , state ini, aplymiddleawere
 const store = createStore(reducerPrincipal);
-//dispatch : forma en qu se cambia el state
 
+console.log(store.getState());
+//dispatch : forma en qu se cambia el state
+store.dispatch({ type: "AGREGAR_USUARIO", nombre: "Michael" });
+console.log(store.getState());
+store.dispatch({ type: "AGREGAR_USUARIO", nombre: "Michael" });
+console.log(store.getState());
+store.dispatch({ type: "MOSTRAR_USUARIOS" });
 console.log(store.getState());
